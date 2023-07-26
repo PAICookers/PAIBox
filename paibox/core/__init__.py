45edb8360fd0440bd3d23fb512b4169d45d74a51
KafCoppelia <k740677208@gmail.com>
2023-07-26 19:52:29 +0800

    add some neuron instances

--- paibox/core/__init__.py
+++ paibox/core/__init__.py
@@ -2,9 +2,12 @@
 from .reg_model import ParamsReg as ParamsReg
 =======
 from .coord import Coord as Coord
-from .neuron import BasicFireNeuron as BasicFireNeuron, PeriodFireNeuron as PeriodFireNeuron
+from .neuron import TonicSpikingNeuron as TonicSpikingNeuron, PhasicSpikingNeuron as PhasicSpikingNeuron, Class1ExcitableNeuron as Class1ExcitableNeuron
 from .neuron.ram_model import ParamsRAM as ParamsRAM
 from .reg_model import ParamsReg as ParamsReg
+<<<<<<< HEAD
 
 __all__ = ["Coord", "BasicFireNeuron", "PeriodFireNeuron", "ParamsReg", "ParamsRAM"]
 >>>>>>> 86a2555 (修改参数定义文件)
+=======
+>>>>>>> d61b789 (add some neuron instances)
