8b9e60a8ef1fe006cd724a6a2db2a286c8ac56a7
KafCoppelia <k740677208@gmail.com>
2023-08-07 14:58:10 +0800

    🐛 fix `_MetaNeuron`

--- paibox/neuron/__init__.py
+++ paibox/neuron/__init__.py
@@ -1,3 +1,4 @@
 from .neuron import Class1ExcitableNeuron as Class1ExcitableNeuron
 from .neuron import PhasicSpikingNeuron as PhasicSpikingNeuron
 from .neuron import TonicSpikingNeuron as TonicSpikingNeuron
+from .neuron import Neuron as Neuron
