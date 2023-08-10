64c6c3ef1bc2bca9847ecc52f23cd230bdaa814c
KafCoppelia <k740677208@gmail.com>
2023-08-10 16:07:16 +0800

    🚧 rebuild neurons

--- paibox/neuron/__init__.py
+++ paibox/neuron/__init__.py
@@ -1,3 +1,3 @@
-from .neuron import PhasicSpikingNeuron as PhasicSpikingNeuron
-from .neuron import TonicSpikingNeuron as TonicSpikingNeuron
-from .neuron import Neuron as Neuron
+from .neurons import PhasicSpikingNeuron as PhasicSpikingNeuron
+from .neurons import TonicSpikingNeuron as TonicSpikingNeuron
+from .neurons import Neuron as Neuron
