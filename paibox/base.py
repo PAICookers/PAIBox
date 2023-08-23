4c28cf37ecea859d5f203df72c9641eecf1ca91f
KafCoppelia <k740677208@gmail.com>
2023-08-23 10:27:32 +0800

    ✨ add `PAIBoxObject`

--- paibox/base.py
+++ paibox/base.py
@@ -1,12 +1,42 @@
-from typing import Any, Optional
-from paibox.general import unique_name
+from typing import Optional
+from paibox.generic import get_unique_name, is_name_unique
 
 
 class PAIBoxObject:
+
     def __init__(self, name: Optional[str] = None) -> None:
-        self.name: str = unique_name(self, name)
+        self._name: str = self.unique_name(name)
+
+    def __eq__(self):
+        pass
+    
+    def unique_name(self, name: Optional[str] = None, _type: Optional[str] = None
+    ) -> str:
+        if name is None:
+            if _type is None:
+                __type = self.__class__.__name__
+            else:
+                __type = _type
+
+            return get_unique_name(__type)
+
+        is_name_unique(name, self)
+        return name
+    
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @name.setter
+    def name(self, name: str) -> None:
+        self._name = self.unique_name(name)
+    
+    
+class StatelessObject(PAIBoxObject):
+    pass
 
 
-class SliceView:
-    def __init__(self, obj: Any, key=slice(None)) -> None:
-        self.obj = obj
+class DynamicSys(PAIBoxObject):
+    
+    pass
