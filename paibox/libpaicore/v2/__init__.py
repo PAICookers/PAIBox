ec25a8121842249d4ddff81725e40fe38c6ea9ee
KafCoppelia <k740677208@gmail.com>
2023-09-19 17:01:06 +0800

    🚚 moved the params & config related to paicore2 to `libpaicore/v2`

---
+++ paibox/libpaicore/v2/__init__.py
@@ -0,0 +1,18 @@
+from .ram_types import (
+    ResetMode as RM,
+    LeakingComparisonMode as LCM,
+    NegativeThresholdMode as NTM,
+    LeakingDirectionMode as LDM,
+    LeakingIntegrationMode as LIM,
+    SynapticIntegrationMode as SIM,
+    ThresholdMode as TM,
+)
+
+from .reg_types import (
+    WeightPrecisionType as WeightPrecisionType,
+    LCNExtensionType as LCN_EX,
+    InputWidthFormatType as InputWidthFormatType,
+    SpikeWidthFormatType as SpikeWidthFormatType,
+    MaxPoolingEnableType as MaxPoolingEnableType,
+    SNNModeEnableType as SNNModeEnableType,
+)
