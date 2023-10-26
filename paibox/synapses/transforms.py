d8bf9c985067ba4360e38b3ee6ba463c40b958a7
birdswimming <72957950+birdswimming@users.noreply.github.com>
2023-10-26 23:55:14 +0800

    optimize transform (#31)
    * optimize transform
    
    * :rotating_light: auto fix by pre-commit hooks
    
    * bug fix
    
    * bug fix
    
    * :rotating_light: auto fix by pre-commit hooks
    
    * :rotating_light: auto fix by pre-commit hooks
    
    ---------
    
    Co-authored-by: KafCoppelia <69038090+KafCoppelia@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- paibox/synapses/transforms.py
+++ paibox/synapses/transforms.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from paibox.utils import is_shape
 
-from .connector import MatConn
+from .connector import MatConn, One2One, TwoEndConnector
 
 
 def _get_dtype(weight: np.ndarray) -> Union[Type[np.bool_], Type[np.int8]]:
@@ -29,14 +29,56 @@ def _get_dtype(weight: np.ndarray) -> Union[Type[np.bool_], Type[np.int8]]:
 
 
 class Transform(ABC):
-    @abstractmethod
+    weights: np.ndarray
+    num_in: int
+    num_out: int
+    mask: np.ndarray
+    conn: TwoEndConnector
+
     def __call__(self, x):
-        raise NotImplementedError
+        return x @ self.weights
 
     @property
     def dtype(self):
         """The dtype of the weight."""
         raise NotImplementedError
+
+    def __init__(
+        self,
+        conn: TwoEndConnector,
+        weights: Union[int, np.integer, np.ndarray],
+    ) -> None:
+        self.conn = conn
+        self.mask = self.conn.build_mat()
+        self.num_in = self.conn.source_num
+        self.num_out = self.conn.dest_num
+        self._init_weights(weights)
+        # Element-wise Multiplication
+        self.weights = np.asarray(weights, dtype=np.int8) * self.mask
+
+    def _init_weights(self, weights: Union[int, np.integer, np.ndarray]) -> None:
+        if isinstance(weights, np.ndarray):
+            if is_shape(weights, (self.num_in, self.num_out)):
+                self.weights = weights
+            elif (
+                is_shape(weights, (self.num_in,))
+                and self.num_in == self.num_out
+                and isinstance(self.conn, One2One)
+            ):
+                # only one to one connection can use a vector as weights
+                self.weights = np.diag(weights)
+            else:
+                raise ValueError
+        elif isinstance(weights, np.integer) or isinstance(weights, int):
+            self.weights = np.full((self.num_in, self.num_out), weights)
+        else:
+            raise ValueError
+        self.weights = self.weights.astype(np.int8)
+
+        if self.weights.shape == self.mask.shape:
+            self.weights = self.weights * self.mask
+        else:
+            raise ValueError
 
     @property
     def connectivity(self) -> np.ndarray:
@@ -44,156 +86,156 @@ class Transform(ABC):
         raise NotImplementedError
 
 
-class OneToOne(Transform):
-    def __init__(self, num: int, weights: Union[int, np.integer, np.ndarray]) -> None:
-        """
-        Arguments:
-            - num: number of neurons.
-            - weights: synaptic weights. The shape must be a scalar or array (num,).
-                If `weights` is a scalar, the connectivity matrix will be:
-                [[x, 0, 0]
-                 [0, x, 0]
-                 [0, 0, x]]
+# class OneToOne(Transform):
+#     def __init__(self, num: int, weights: Union[int, np.integer, np.ndarray]) -> None:
+#         """
+#         Arguments:
+#             - num: number of neurons.
+#             - weights: synaptic weights. The shape must be a scalar or array (num,).
+#                 If `weights` is a scalar, the connectivity matrix will be:
+#                 [[x, 0, 0]
+#                  [0, x, 0]
+#                  [0, 0, x]]
 
-                Or `weights` is an array, [x, y, z] corresponding to the weights of \
-                    the post-neurons respectively. The connectivity matrix will be:
-                [[x, 0, 0]
-                 [0, y, 0]
-                 [0, 0, z]]
-        """
-        self.num = num
+#                 Or `weights` is an array, [x, y, z] corresponding to the weights of \
+#                     the post-neurons respectively. The connectivity matrix will be:
+#                 [[x, 0, 0]
+#                  [0, y, 0]
+#                  [0, 0, z]]
+#         """
+#         self.num = num
 
-        if isinstance(weights, np.ndarray) and not is_shape(weights, (num,)):
-            # TODO Error description
-            raise ValueError
+#         if isinstance(weights, np.ndarray) and not is_shape(weights, (num,)):
+#             # TODO Error description
+#             raise ValueError
 
-        self.weights = np.asarray(weights, dtype=np.int8)
+#         self.weights = np.asarray(weights, dtype=np.int8)
 
-    def __call__(self, x: np.ndarray) -> np.ndarray:
-        return x * self.weights
+#     def __call__(self, x: np.ndarray) -> np.ndarray:
+#         return x * self.weights
 
-    @property
-    def dtype(self) -> Union[Type[np.bool_], Type[np.int8]]:
-        return _get_dtype(self.weights)
+#     @property
+#     def dtype(self) -> Union[Type[np.bool_], Type[np.int8]]:
+#         return _get_dtype(self.weights)
 
-    @property
-    def connectivity(self) -> np.ndarray:
-        return (
-            self.weights.astype(self.dtype)
-            if self.weights.ndim == 2
-            else (self.weights * np.eye(self.num, dtype=np.bool_)).astype(self.dtype)
-        )
-
-
-class ByPass(OneToOne):
-    def __init__(self, num: int) -> None:
-        """
-        Arguments:
-            - num: number of neurons.
-
-        NOTE: The weights are always 1.
-        """
-        super().__init__(num, 1)
+#     @property
+#     def connectivity(self) -> np.ndarray:
+#         return (
+#             self.weights.astype(self.dtype)
+#             if self.weights.ndim == 2
+#             else (self.weights * np.eye(self.num, dtype=np.bool_)).astype(self.dtype)
+#         )
 
 
-class AllToAll(Transform):
-    def __init__(
-        self, num_in: int, num_out: int, weights: Union[int, np.integer, np.ndarray]
-    ) -> None:
-        """
-        Arguments:
-            - num_in: number of source neurons.
-            - num_out: number of destination neurons.
-            - weights: synaptic weights. The shape must be a scalar or a matrix.
-                If `weights` is a scalar, the connectivity matrix will be:
-                [[x, x, x]
-                 [x, x, x]
-                 [x, x, x]]
+# class ByPass(OneToOne):
+#     def __init__(self, num: int) -> None:
+#         """
+#         Arguments:
+#             - num: number of neurons.
 
-                Or `weights` is a matrix, then the connectivity matrix will be:
-                [[a, b, c]
-                 [d, e, f]
-                 [g, h, i]]
-        """
-        self.num_in = num_in
-        self.num_out = num_out
-
-        if isinstance(weights, np.ndarray) and not is_shape(weights, (num_in, num_out)):
-            # TODO Error description
-            raise ValueError
-
-        self.weights = np.asarray(weights, dtype=np.int8)
-
-    def __call__(self, x: np.ndarray) -> np.ndarray:
-        """
-        When weights is a scalar, the output is a scalar.
-        When weights is a matrix, the output is the dot product of `x` and `weights`.
-        """
-        if self.weights.ndim == 0:
-            # weight is a scalar
-            if x.ndim == 1:
-                _x = np.sum(x).astype(np.int32)
-                output = self.weights * _x
-            else:
-                # TODO
-                raise ValueError
-
-        elif self.weights.ndim == 2:
-            output = x @ self.weights
-        else:
-            raise ValueError(f"weights.ndim={self.weights.ndim}")
-
-        return output
-
-    @property
-    def dtype(self) -> Union[Type[np.bool_], Type[np.int8]]:
-        return _get_dtype(self.weights)
-
-    @property
-    def connectivity(self) -> np.ndarray:
-        return (
-            self.weights.astype(self.dtype)
-            if self.weights.ndim == 2
-            else (self.weights * np.ones((self.num_in, self.num_out), np.bool_)).astype(
-                self.dtype
-            )
-        )
+#         NOTE: The weights are always 1.
+#         """
+#         super().__init__(num, 1)
 
 
-class MaskedLinear(Transform):
-    def __init__(
-        self,
-        conn: MatConn,
-        weights: Union[int, np.integer, np.ndarray],
-    ) -> None:
-        """
-        Arguments:
-            - conn: connector. Only support `MatConn`.
-            - weights: unmasked weights.
+# class AllToAll(Transform):
+#     def __init__(
+#         self, num_in: int, num_out: int, weights: Union[int, np.integer, np.ndarray]
+#     ) -> None:
+#         """
+#         Arguments:
+#             - num_in: number of source neurons.
+#             - num_out: number of destination neurons.
+#             - weights: synaptic weights. The shape must be a scalar or a matrix.
+#                 If `weights` is a scalar, the connectivity matrix will be:
+#                 [[x, x, x]
+#                  [x, x, x]
+#                  [x, x, x]]
 
-        TODO to be varified.
-        """
-        self.conn = conn
-        self.mask = self.conn.build_mat()
-        self.num_in = self.conn.source_num
-        self.num_out = self.conn.dest_num
+#                 Or `weights` is a matrix, then the connectivity matrix will be:
+#                 [[a, b, c]
+#                  [d, e, f]
+#                  [g, h, i]]
+#         """
+#         self.num_in = num_in
+#         self.num_out = num_out
 
-        if isinstance(weights, np.ndarray) and not is_shape(
-            weights, (self.num_in, self.num_out)
-        ):
-            # TODO Error description
-            raise ValueError
+#         if isinstance(weights, np.ndarray) and not is_shape(weights, (num_in, num_out)):
+#             # TODO Error description
+#             raise ValueError
 
-        # Element-wise Multiplication
-        self.weights = np.asarray(weights, dtype=np.int8) * self.mask
+#         self.weights = np.asarray(weights, dtype=np.int8)
 
-    def __call__(self, x: np.ndarray) -> np.ndarray:
-        return x @ self.weights
+#     def __call__(self, x: np.ndarray) -> np.ndarray:
+#         """
+#         When weights is a scalar, the output is a scalar.
+#         When weights is a matrix, the output is the dot product of `x` and `weights`.
+#         """
+#         if self.weights.ndim == 0:
+#             # weight is a scalar
+#             if x.ndim == 1:
+#                 _x = np.sum(x).astype(np.int32)
+#                 output = self.weights * _x
+#             else:
+#                 # TODO
+#                 raise ValueError
 
-    @property
-    def dtype(self) -> Union[Type[np.bool_], Type[np.int8]]:
-        return _get_dtype(self.weights)
+#         elif self.weights.ndim == 2:
+#             output = x @ self.weights
+#         else:
+#             raise ValueError(f"weights.ndim={self.weights.ndim}")
 
-    @property
-    def connectivity(self) -> np.ndarray:
-        return self.weights.astype(self.dtype)
+#         return output
+
+#     @property
+#     def dtype(self) -> Union[Type[np.bool_], Type[np.int8]]:
+#         return _get_dtype(self.weights)
+
+#     @property
+#     def connectivity(self) -> np.ndarray:
+#         return (
+#             self.weights.astype(self.dtype)
+#             if self.weights.ndim == 2
+#             else (self.weights * np.ones((self.num_in, self.num_out), np.bool_)).astype(
+#                 self.dtype
+#             )
+#         )
+
+
+# class MaskedLinear(Transform):
+#     def __init__(
+#         self,
+#         conn: MatConn,
+#         weights: Union[int, np.integer, np.ndarray],
+#     ) -> None:
+#         """
+#         Arguments:
+#             - conn: connector. Only support `MatConn`.
+#             - weights: unmasked weights.
+
+#         TODO to be varified.
+#         """
+#         self.conn = conn
+#         self.mask = self.conn.build_mat()
+#         self.num_in = self.conn.source_num
+#         self.num_out = self.conn.dest_num
+
+#         if isinstance(weights, np.ndarray) and not is_shape(
+#             weights, (self.num_in, self.num_out)
+#         ):
+#             # TODO Error description
+#             raise ValueError
+
+#         # Element-wise Multiplication
+#         self.weights = np.asarray(weights, dtype=np.int8) * self.mask
+
+#     def __call__(self, x: np.ndarray) -> np.ndarray:
+#         return x @ self.weights
+
+#     @property
+#     def dtype(self) -> Union[Type[np.bool_], Type[np.int8]]:
+#         return _get_dtype(self.weights)
+
+#     @property
+#     def connectivity(self) -> np.ndarray:
+#         return self.weights.astype(self.dtype)
