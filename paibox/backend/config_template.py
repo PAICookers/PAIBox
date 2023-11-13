8c5dd09af8469ff373af95d7c0396708fe2cd2eb
KafCoppelia <k740677208@gmail.com>
2023-11-13 23:44:56 +0800

    🧱 added basic objects

--- paibox/backend/config_template.py
+++ paibox/backend/config_template.py
@@ -9,22 +9,20 @@ from paibox.libpaicore import (
     LCN_EX,
     AxonCoord,
     Coord,
+    CoordLike,
     InputWidthFormat,
+    HwConfig,
     MaxPoolingEnable,
     NeuronAttrs,
     NeuronDestInfo,
+    ParamsRAM,
     ParamsReg,
     SNNModeEnable,
     SpikeWidthFormat,
     WeightPrecision,
     get_replication_id,
+    to_coord,
 )
-from paibox.libpaicore.v2.coordinate import CoordLike, to_coord
-
-try:
-    import ujson as json
-except ModuleNotFoundError:
-    import json
 
 
 class GlobalConfig:
@@ -37,51 +35,6 @@ class GlobalConfig:
     @test_chip_addr.setter
     def test_chip_addr(self, addr: CoordLike) -> None:
         self.TEST_CHIP_ADDR = to_coord(addr)
-
-
-class ConfigTemplate:
-    """A configuration template."""
-
-    frame_type: ClassVar[FrameType] = FrameType.FRAME_CONFIG
-
-
-@dataclass(eq=False)
-class NeuronConfig(ConfigTemplate):
-    attr: NeuronAttrs
-    addr_ram: slice
-    dest_info: NeuronDestInfo
-
-    @classmethod
-    def build(
-        cls,
-        neuron: NeuDyn,
-        addr_ram: slice,
-        axon_coords: List[AxonCoord],
-        dest_coords: List[Coord],
-    ):
-        """Build the `NeuronConfig`.
-
-        Args:
-            - neuron: the target `NeuDyn`.
-            - addr_ram: the assigned RAM address of the target neuron.
-            - axon_segs: the destination axon segments.
-            - dest_coords: the coordinates of destination axons.
-        """
-        attr = NeuronAttrs(**neuron.export_params())
-        axon_rid = get_replication_id(dest_coords)
-
-        dest_info_dict = {
-            "tick_relative": [coord.tick_relative for coord in axon_coords],
-            "addr_axon": [coord.addr_axon for coord in axon_coords],
-            "addr_core_x": dest_coords[0].x,
-            "addr_core_y": dest_coords[0].y,
-            "addr_core_x_ex": axon_rid.x,
-            "addr_core_y_ex": axon_rid.y,
-            "addr_chip_x": 0,
-            "addr_chip_y": 0,
-        }
-        return dest_info_dict
-        # return cls(attr, addr_ram, NeuronDestInfo(**dest_info_dict))
 
 
 class CoreConfigDict(NamedTuple):
@@ -98,6 +51,65 @@ class CoreConfigDict(NamedTuple):
     snn_mode_en: SNNModeEnable
     target_lcn: LCN_EX
     test_chip_addr: int
+
+
+class ConfigTemplate:
+    """A configuration template."""
+
+    frame_type: ClassVar[FrameType] = FrameType.FRAME_CONFIG
+
+
+@dataclass(eq=False)
+class NeuronConfig(ConfigTemplate):
+    addr_ram: List[int]
+    addr_offset: int
+    params_ram: ParamsRAM
+
+    @classmethod
+    def build(
+        cls,
+        neuron: NeuDyn,
+        addr_ram: List[int],
+        addr_offset: int,
+        axon_coords: List[AxonCoord],
+        dest_coords: List[Coord],
+    ):
+        """Build the `NeuronConfig`.
+
+        Args:
+            - neuron: the target `NeuDyn`.
+            - addr_ram: the assigned RAM address of the target neuron.
+            - addr_offset: the offset of the RAM address.
+            - axon_segs: the destination axon segments.
+            - dest_coords: the coordinates of destination axons.
+        """
+        attrs = NeuronAttrs.model_validate(neuron.export_params(), strict=True)
+        axon_rid = get_replication_id(dest_coords)
+
+        dest_info_dict = {
+            "tick_relative": [coord.tick_relative for coord in axon_coords],
+            "addr_axon": [coord.addr_axon for coord in axon_coords],
+            "addr_core_x": dest_coords[0].x,
+            "addr_core_y": dest_coords[0].y,
+            "addr_core_x_ex": axon_rid.x,
+            "addr_core_y_ex": axon_rid.y,
+            "addr_chip_x": 0,
+            "addr_chip_y": 0,
+        }
+
+        neuron_dest_info = NeuronDestInfo.model_validate(dest_info_dict, strict=True)
+
+        return cls(
+            addr_ram,
+            addr_offset,
+            ParamsRAM(attrs=attrs, dest_info=neuron_dest_info),
+        )
+
+    def export_params(self):
+        dict_ = {"addr_ram": self.addr_ram, "addr_offset": self.addr_offset}
+        dict_ |= self.params_ram.model_dump(by_alias=True)
+
+        return dict_
 
 
 @dataclass(eq=False)
@@ -119,7 +131,7 @@ class CoreConfig(ConfigTemplate):
             coord,
             core_config.random_seed,
             core_config.weight_ram,
-            ParamsReg(**core_config._asdict()),
+            ParamsReg.model_validate(**core_config._asdict(), strict=True),
             neuron_ram,
         )
 
@@ -127,8 +139,10 @@ class CoreConfig(ConfigTemplate):
         return self.random_seed
 
     def export_type2(self):
-        model = ParamsReg.model_validate(self.params_reg, strict=True)
-        return model.model_dump(by_alias=True)
+        return self.params_reg.model_dump(by_alias=True)
+
+    def export_type3(self):
+        raise NotImplementedError
 
     def export_type4(self):
         return self.weight_ram
