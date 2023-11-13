7f708ee6788f8f4f8c96595808c82ddce89a3140
KafCoppelia <k740677208@gmail.com>
2023-11-13 23:42:36 +0800

    🚧 updated `hw_types` & added comments

--- paibox/libpaicore/v2/hw_types.py
+++ paibox/libpaicore/v2/hw_types.py
@@ -1,20 +1,58 @@
-from typing import NamedTuple
-
+from typing import List, NamedTuple
+from .hw_defs import HwConfig
 
 __all__ = ["NeuronSegment", "AxonCoord", "AxonSegment"]
 
 
 class NeuronSegment(NamedTuple):
+    """Segment of neuron.
+
+    index =(0, 100, 1)
+    addr_offset = 0
+    interval = 2
+
+    The address of RAM will be: slice(0, 200, 2)
+    """
+
     index: slice
     """The original index of this segment of neurons."""
     addr_offset: int
     """The offset of the RAM address."""
 
+    interval: int = 1
+    """The interval of address when mapping neuron attributes on the RAM."""
+
+    weight_steps: int = 1
+    """Reserved."""
+
     @property
-    def addr_ram(self) -> slice:
+    def n_neuron(self) -> int:
+        return self.index.stop - self.index.start
+
+    @property
+    def nrange(self) -> slice:
+        if (
+            self.addr_offset + self.interval * (self.index.stop - self.index.start)
+            > HwConfig.N_NEURON_ONE_CORE_MAX
+        ):
+            # TODO
+            raise AttributeError
+
         return slice(
-            self.addr_offset, self.addr_offset + self.index.stop - self.index.start
+            self.addr_offset,
+            self.addr_offset + (self.index.stop - self.index.start),
+            1,
         )
+
+    @property
+    def addr_ram(self) -> List[int]:
+        """Convert index of neuron into address RAM."""
+        addr_ram = []
+
+        for addr in range(HwConfig.N_NEURON_ONE_CORE_MAX)[self.nrange]:
+            addr_ram.extend([addr] * self.interval)
+
+        return addr_ram
 
 
 class AxonCoord(NamedTuple):
