29f42699e2693a6ba233bd89618a4c0beddff36d
KafCoppelia <k740677208@gmail.com>
2023-11-17 23:42:45 +0800

    ✅ added tests for graphs

--- tests/backend/test_graphs.py
+++ tests/backend/test_graphs.py
@@ -1,6 +1,7 @@
 import pytest
 
-from paibox.backend.graphs import get_node_degrees, group_edges_proto, toposort
+from paibox.backend.graphs import *
+from paibox.exceptions import NotSupportedError
 
 
 class TestTopoSort:
@@ -15,7 +16,6 @@ class TestTopoSort:
                     "n3": {},
                     "n4": {"n2"},
                 }
-                # ["inp1", "n1", "n4", "n2", "n3"],
             ),
             (
                 {
@@ -28,7 +28,6 @@ class TestTopoSort:
                     "n6": {"n7"},
                     "n7": {"n4"},
                 }
-                # ["inp1", "n1", "n5", "n2", "n3", "n6", "n7", "n4"],
             ),
             (
                 {
@@ -51,28 +50,31 @@ class TestTopoSort:
                 }
             ),
         ],
-        ids=["one_input_1", "one_input_2", "multi_inputs_1", "one_input_3_branches"],
+        ids=["one_input_1", "one_input_2", "multi_inputs_1", "one_input_3"],
     )
-    def test_toposort_abstract(self, edges):
+    def test_toposort(self, edges):
         """
-        Test #1: one input
-        INP1 -> N1 -> N2 -> N3
-        N1 -> N4
-        N4 -> N2
+        Test #1: one input 1
+            INP1 -> N1 -> N2 -> N3
+            N1 -> N4
+            N4 -> N2
 
-        Test #2: one input
-        INP1 -> N1 -> N2 -> N3 -> N4
-        N1 -> N5 -> N6 -> N7 -> N4
-        N5 -> N3
-        N3 -> N6
+        Test #2: one input 2
+            INP1 -> N1 -> N2 -> N3 -> N4
+            N1 -> N5 -> N6 -> N7 -> N4
+            N5 -> N3
+            N3 -> N6
 
-        Test #2: more than one input
-        INP1 -> N1 -> N2 -> N3
-        INP2 -> N4 -> N5 -> N3
-        N2 -> N4
+        Test #3: more than one input
+            INP1 -> N1 -> N2 -> N3
+            INP2 -> N4 -> N5 -> N3
+            N2 -> N4
+
+        Test #4: one input 3
+            INP1 -> N1 -> N2 -> N4
+            N1 -> N3 -> N4
         """
         ordered = toposort(edges)
-
         assert len(ordered) == len(edges)
 
     @pytest.mark.parametrize(
@@ -118,61 +120,69 @@ class TestTopoSort:
         INP2 -> N2 -> N1
         INP3 -> N2
         """
-        with pytest.raises(ValueError):
-            ordered = toposort(edges)
+        with pytest.raises(NotSupportedError):
+            ordered = toposort(edges, is_strict=True)
 
 
 class TestGroupEdges:
     @pytest.mark.parametrize(
-        "nodes, edges, succ_edges",
+        "edges, succ_edges",
         [
             (
-                ["inp1", "n1", "n2", "n3"],
-                ["s1", "s2", "s3", "s4"],
+                ["s1", "s2", "s3", "s4", "s5"],
                 {
                     "inp1": {"n1": "s1"},
-                    "n1": {"n2": "s2"},
-                    "n2": {"n1": "s3", "n3": "s4"},
+                    "n1": {"n2": "s2", "n4": "s3"},
+                    "n2": {"n3": "s4"},
                     "n3": {},
+                    "n4": {"n2": "s5"},
                 },
             ),
             (
-                ["inp1", "n1", "n2", "n3", "n4", "n5", "n6"],
-                ["s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"],
+                ["s1", "s2", "s3", "s4", "s5"],
                 {
                     "inp1": {"n1": "s1"},
                     "n1": {"n2": "s2", "n3": "s3"},
-                    "n2": {"n3": "s4"},
-                    "n3": {"n4": "s5", "n5": "s6"},
-                    "n4": {"n6": "s7"},
-                    "n5": {"n6": "s8"},
-                    "n6": {},
+                    "n2": {"n4": "s4"},
+                    "n3": {"n4": "s5"},
+                    "n4": {},
                 },
             ),
             (
-                ["n1", "n2", "n3", "n4", "n5"],
-                ["s1", "s2", "s3", "s4"],
+                ["s1", "s2", "s3", "s4", "s5", "s6"],
                 {
-                    "n1": {"n3": "s1", "n4": "s2"},
-                    "n2": {"n4": "s3", "n5": "s4"},
+                    "inp1": {"n1": "s1"},
+                    "inp2": {"n4": "s2"},
+                    "n1": {"n2": "s3"},
+                    "n2": {"n3": "s4"},
                     "n3": {},
-                    "n4": {},
-                    "n5": {},
+                    "n4": {"n5": "s5"},
+                    "n5": {"n3": "s6"},
                 },
             ),
         ],
+        ids=["topo_1", "topo_2", "topo_3"],
     )
-    def test_group_edges_proto(self, nodes, edges, succ_edges):
+    def test_group_edges_ordered(self, edges, succ_edges):
         """
         Test #1:
-            INP1 -> S1 -> N1 -> S2 -> N2 -> S4 -> N3
-                          | <-- S3 <- |
+            INP1 -> N1 -> N2 -> N3
+            N1 -> N4
+            N4 -> N2
 
         Test #2:
-            INP1 -> S1 -> N1 -> S2 -> N2 -> S4 -> N3 -> S5 -> N4 -> S7 -> N6
-                           | -> S3 --------------> | -> S6 -> N5 -> S8 -->|
+            INP1 -> N1 -> N2 -> N4
+            N1 -> N3 -> N4
+
+        Test #3:
+            INP1 -> N1 -> N2 -> N3
+            INP2 -> N4 -> N5 -> N3
+            N2 -> N3
         """
-        degree, gathered = group_edges_proto(nodes, edges, succ_edges)
+        degrees = get_node_degrees(succ_edges)
+        ordered_nodes = toposort(succ_edges)
+        gathered = group_edges(edges, succ_edges, degrees, ordered_nodes=ordered_nodes)
+        # gathered = group_edges(edges, succ_edges, degrees, ordered_nodes=None)
 
         print()
 
