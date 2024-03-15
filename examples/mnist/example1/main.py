3bb381e2e49fd6e8175e23401396f22a97b342eb
KafCoppelia <k740677208@gmail.com>
2024-03-15 16:31:43 +0800

    📝 updated example1 of MNIST

--- examples/mnist/example1/main.py
+++ examples/mnist/example1/main.py
@@ -1,14 +1,59 @@
 import argparse
-from pathlib import Path
-
 import numpy as np
-from model import fcnet_2layer_dual_port
+from pathlib import Path
 
 import paibox as pb
 
 
+class fcnet_2layer_dual_port(pb.Network):
+    def __init__(self, weight1, Vthr1, weight2, Vthr2):
+        super().__init__()
+
+        pe = pb.simulator.PoissonEncoder()
+        self.i1 = pb.InputProj(input=pe, shape_out=(392,))
+        self.i2 = pb.InputProj(input=pe, shape_out=(392,))
+        self.n1 = pb.IF(128, threshold=Vthr1, reset_v=0)
+        self.s1 = pb.FullConn(
+            self.i1,
+            self.n1,
+            weights=weight1[:392],
+            conn_type=pb.SynConnType.All2All,
+        )
+        self.s2 = pb.FullConn(
+            self.i2,
+            self.n1,
+            weights=weight1[392:],
+            conn_type=pb.SynConnType.All2All,
+        )
+
+        # tick_wait_start = 2 for second layer
+        self.n2 = pb.IF(
+            5, threshold=Vthr2, reset_v=0, tick_wait_start=2, name="batch_dual_port_o1"
+        )
+        self.n3 = pb.IF(
+            5, threshold=Vthr2, reset_v=0, tick_wait_start=2, name="batch_dual_port_o2"
+        )
+        self.s3 = pb.FullConn(
+            self.n1,
+            self.n2,
+            weights=weight2[:, :5],
+            conn_type=pb.SynConnType.All2All,
+        )
+        self.s4 = pb.FullConn(
+            self.n1,
+            self.n3,
+            weights=weight2[:, 5:],
+            conn_type=pb.SynConnType.All2All,
+        )
+
+        self.probe1 = pb.Probe(target=self.n2, attr="spike")
+        self.probe2 = pb.Probe(target=self.n3, attr="spike")
+
+
+param_dict = {}
+
+
 def getNetParam():
-    param_dict = {}
     timestep = 4
     layer_num = 2
     delay = layer_num - 1
@@ -27,8 +72,6 @@ def getNetParam():
     param_dict["w2"] = w2
     param_dict["vthr2"] = vthr2
 
-    return param_dict
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -37,7 +80,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    param_dict = getNetParam()
+    getNetParam()
     pb_net = fcnet_2layer_dual_port(
         param_dict["w1"],
         param_dict["vthr1"],
@@ -46,7 +89,7 @@ if __name__ == "__main__":
     )
 
     # Network simulation
-    raw_data = np.load("./data/mnist_input_data7.npy")
+    raw_data = np.load(Path(__file__).parent.parent / "data" / "mnist_input_data7.npy")
     input_data = raw_data.flatten()
 
     # Visualize
@@ -79,6 +122,10 @@ if __name__ == "__main__":
     graph_info = mapper.compile(
         weight_bit_optimization=True, grouping_optim_target="both"
     )
+
+    # #N of cores required
+    print("Core required:", graph_info["n_core_required"])
+
     mapper.export(
         write_to_file=True,
         fp=out_dir / "debug",
