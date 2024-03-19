2809451ff8abd0e90ad682fa0b3fad81bf3650dd
KafCoppelia <k740677208@gmail.com>
2024-03-19 16:45:26 +0800

    ✨ added Always1Neuron

--- paibox/neuron/__init__.py
+++ paibox/neuron/__init__.py
@@ -3,10 +3,6 @@ from .neurons import IF as IF
 from .neurons import LIF as LIF
 from .neurons import PhasicSpiking as PhasicSpiking
 from .neurons import TonicSpiking as TonicSpiking
+from .neurons import Always1Neuron as Always1Neuron
 
-__all__ = [
-    "IF",
-    "LIF",
-    "TonicSpiking",
-    "PhasicSpiking",
-]
+__all__ = ["IF", "LIF", "TonicSpiking", "PhasicSpiking", "Always1Neuron"]
