313cd03f802e3d5b6d46fa33ca2a3679ab790001
KafCoppelia <k740677208@gmail.com>
2024-08-31 14:17:20 +0800

    🚚 move conv golden ref functions to utils

--- tests/components/synapses/test_transforms.py
+++ tests/components/synapses/test_transforms.py
@@ -6,6 +6,8 @@ from paibox.exceptions import AutoOptimizationWarning
 from paibox.types import WEIGHT_DTYPE
 from paibox.utils import shape2num
 
+from ..utils import _conv1d_golden, _conv2d_golden
+
 
 class TestTransforms:
     @pytest.mark.parametrize(
@@ -238,41 +240,6 @@ class TestTransforms:
         assert np.array_equal(y2, expected.ravel())
         assert f.connectivity.shape == (x.size, y.size)
 
-    @staticmethod
-    def _conv1d_golden(
-        x: np.ndarray,
-        out_shape: tuple[int],
-        kernel: np.ndarray,
-        stride: tuple[int],
-        padding: tuple[int],
-    ):
-        cout, cin, kl = kernel.shape
-        xcin, il = x.shape
-
-        assert cin == xcin
-
-        ol = (il - kl + 2 * padding[0]) // stride[0] + 1
-
-        assert ol == out_shape[0]
-
-        out = np.zeros((cout,) + out_shape, dtype=np.int64)
-
-        x_padded = np.pad(x, ((0, 0), (padding[0], padding[0])), mode="constant")
-        conv_result = np.zeros((ol,), dtype=np.int64)
-
-        for o in range(cout):
-            for i in range(cin):
-                conv_result.fill(0)
-                for l in range(ol):
-                    window = x_padded[i, l * stride[0] : l * stride[0] + kl].astype(
-                        np.int64
-                    )
-                    conv_result[l] = np.sum(window * kernel[o, i, :])
-
-                out[o] += conv_result
-
-        return out
-
     @pytest.mark.parametrize(
         "xdtype, in_shape, in_channels, out_channels, kernel_size, stride, padding, kdtype",
         [
@@ -346,7 +313,7 @@ class TestTransforms:
         # The result of matmul using the unrolled matrix
         y2 = xf @ f.connectivity.astype(np.int32)
 
-        expected = self._conv1d_golden(x, out_shape, kernel, stride, padding)
+        expected = _conv1d_golden(x, out_shape, kernel, stride, padding)
 
         assert np.array_equal(y1, expected)
         assert np.array_equal(y2, expected.ravel())
@@ -354,49 +321,6 @@ class TestTransforms:
             shape2num((kernel.shape[1],) + in_shape),
             shape2num((kernel.shape[0],) + out_shape),
         )
-
-    @staticmethod
-    def _conv2d_golden(
-        x: np.ndarray,
-        out_shape: tuple[int, int],
-        kernel: np.ndarray,
-        stride: tuple[int, int],
-        padding: tuple[int, int],
-    ):
-        cout, cin, kh, kw = kernel.shape
-        xcin, ih, iw = x.shape
-
-        assert cin == xcin
-
-        oh = (ih - kh + 2 * padding[0]) // stride[0] + 1
-        ow = (iw - kw + 2 * padding[1]) // stride[1] + 1
-
-        assert oh, ow == out_shape
-
-        out = np.zeros((cout,) + out_shape, dtype=np.int64)
-
-        x_padded = np.pad(
-            x,
-            ((0, 0), (padding[0], padding[0]), (padding[1], padding[1])),
-            mode="constant",
-        )
-        conv_result = np.zeros((oh, ow), dtype=np.int64)
-
-        for o in range(cout):
-            for i in range(cin):
-                conv_result.fill(0)
-                for h in range(oh):
-                    for w in range(ow):
-                        window = x_padded[
-                            i,
-                            h * stride[0] : h * stride[0] + kh,
-                            w * stride[1] : w * stride[1] + kw,
-                        ].astype(np.int64)
-                        conv_result[h, w] = np.sum(window * kernel[o, i, :, :])
-
-                out[o] += conv_result
-
-        return out
 
     @pytest.mark.parametrize(
         "xdtype, in_shape, in_channels, out_channels, kernel_size, stride, padding, kdtype",
@@ -469,7 +393,7 @@ class TestTransforms:
         # The result of matmul using the unrolled matrix
         y2 = xf @ f.connectivity.astype(np.int32)
 
-        expected = self._conv2d_golden(x, out_shape, kernel, stride, padding)
+        expected = _conv2d_golden(x, out_shape, kernel, stride, padding)
 
         assert np.array_equal(y1, expected)
         assert np.array_equal(y2, expected.ravel())
