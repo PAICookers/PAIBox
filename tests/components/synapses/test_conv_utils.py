ec8b49c549390a26bdb8f182f7896af5f2cb0f73
KafCoppelia <k740677208@gmail.com>
2025-07-15 20:36:32 +0800

    ✅ tests: add conv unroll tests & skip perf tests in ci

--- tests/components/synapses/test_conv_utils.py
+++ tests/components/synapses/test_conv_utils.py
@@ -3,13 +3,16 @@ import pytest
 import timeit
 
 from paibox.components.synapses.conv_utils import (
+    _conv1d_oshape,
+    _conv2d_oshape,
     _conv1d_unroll,
     _conv2d_unroll,
     conv1d_faster,
     conv2d_faster,
 )
 from tests.conftest import ParametrizedTestData
-from tests.utils import gen_random_array
+from tests.components.utils import conv1d_golden, conv2d_golden
+from tests.utils import gen_random_array, is_ci_env
 
 
 try:
@@ -21,10 +24,10 @@ except ImportError:
 
 
 test_conv1d_faster_data = ParametrizedTestData(
-    args="in_shape, in_channels, out_channels, ksize, stride, padding, dilation, groups",
+    args="in_shape, co, ksize, stride, padding, dilation, groups",
     data=[
-        ((2048,), 16, 32, (128,), (20,), (50,), (8,), 1),
-        ((512,), 64, 32, (16,), (8,), (0,), (1,), 4),
+        ((16, 2048), 32, (128,), (20,), (50,), (8,), 1),
+        ((64, 512), 32, (16,), (8,), (0,), (1,), 4),
     ],
 )
 
@@ -33,45 +36,36 @@ test_conv1d_faster_data = ParametrizedTestData(
     skip_conv1d_faster_test,
     reason="Legacy function 'conv1d_faster_legacy' is removed",
 )
-@pytest.mark.perf
 @pytest.mark.parametrize(
     test_conv1d_faster_data["args"], test_conv1d_faster_data["data"]
 )
-def test_conv1d_faster_perf(
-    in_shape,
-    in_channels,
-    out_channels,
-    ksize,
-    stride,
-    padding,
-    dilation,
-    groups,
-    fixed_rng,
-    request,
-):
-    cin_per_grp = in_channels // groups
+def test_conv1d_perf(in_shape, co, ksize, stride, padding, dilation, groups, fixed_rng):
+    ci = in_shape[0]
+    oshape = _conv1d_oshape(in_shape[1:], ksize, stride, padding, dilation)
+    assert ci % groups == 0 and co % groups == 0
+    ci_in_grp = ci // groups
 
-    x = gen_random_array((in_channels,) + in_shape, np.uint8, fixed_rng)
-    kernel = gen_random_array((out_channels, cin_per_grp) + ksize, np.int8, fixed_rng)
-
-    out_shape = (
-        (in_shape[0] + 2 * padding[0] - dilation[0] * (ksize[0] - 1) - 1) // stride[0]
-        + 1,
-    )
+    x = gen_random_array(in_shape, np.uint8, fixed_rng)
+    kernel = gen_random_array((co, ci_in_grp) + ksize, np.int8, fixed_rng)
 
     def run_conv1d_faster():
-        return conv1d_faster(x, out_shape, kernel, stride, padding, dilation, groups)
+        return conv1d_faster(x, oshape, kernel, stride, padding, dilation, groups)
 
     def run_conv1d_faster_legacy():
         return conv1d_faster_legacy(
-            x, out_shape, kernel, stride, padding, dilation, groups
+            x, oshape, kernel, stride, padding, dilation, groups
         )
 
-    if not request.node.get_closest_marker("perf"):
+    def run_conv1d_golden():
+        return conv1d_golden(x, oshape, kernel, stride, padding, dilation, groups)
+
+    if not is_ci_env():
         # ~10x faster
-        t1 = timeit.timeit(lambda: run_conv1d_faster(), number=10)
-        t2 = timeit.timeit(lambda: run_conv1d_faster_legacy(), number=10)
-        print("Use im2col: ", t1, "Use legacy:", t2)
+        n = 10
+        t1 = timeit.timeit(lambda: run_conv1d_faster(), number=n)
+        t2 = timeit.timeit(lambda: run_conv1d_faster_legacy(), number=n)
+        t3 = timeit.timeit(lambda: run_conv1d_golden(), number=n)
+        print("Optimized: ", t1 / n, "Legacy: ", t2 / n, "Golden: ", t3 / n)
 
     r_opt = run_conv1d_faster()
     r_legacy = run_conv1d_faster_legacy()
@@ -87,11 +81,12 @@ except ImportError:
 
 
 test_conv1d_unroll_data = ParametrizedTestData(
-    args="in_shape, in_channels, out_channels, ksize, stride, padding, groups",
+    args="in_shape, ci, co, ksize, stride, padding, groups",
     data=[
         ((32,), 8, 16, (3,), (1,), (2,), 1),
-        ((240,), 16, 32, (40,), (5,), (0,), 2),
-        ((1000,), 32, 64, (100,), (20,), (20,), 1),
+        ((240,), 16, 32, (40,), (5,), (0,), 1),
+        ((480,), 16, 32, (32,), (16,), (8,), 4),
+        ((1000,), 32, 64, (100,), (20,), (20,), 2),
     ],
 )
 
@@ -100,43 +95,59 @@ test_conv1d_unroll_data = ParametrizedTestData(
     skip_conv1d_unroll_test,
     reason="Legacy function '_conv1d_unroll_legacy' is removed",
 )
-@pytest.mark.perf
 @pytest.mark.parametrize(
     test_conv1d_unroll_data["args"], test_conv1d_unroll_data["data"]
 )
 def test_conv1d_unroll_perf(
-    in_shape,
-    in_channels,
-    out_channels,
-    ksize,
-    stride,
-    padding,
-    groups,
-    fixed_rng,
-    request,
+    in_shape, ci, co, ksize, stride, padding, groups, fixed_rng
 ):
-    out_shape = ((in_shape[0] + 2 * padding[0] - ksize[0]) // stride[0] + 1,)
+    oshape = _conv1d_oshape(in_shape, ksize, stride, padding)
+    assert ci % groups == 0 and co % groups == 0
+    ci_in_grp = ci // groups
 
-    k_shape = (out_channels, in_channels) + ksize
+    k_shape = (co, ci_in_grp) + ksize
     kernel = gen_random_array(k_shape, np.int8, fixed_rng)
 
     def run_conv1d_unroll():
-        return _conv1d_unroll(in_shape, out_shape, kernel, stride, padding, groups)
+        return _conv1d_unroll(in_shape, oshape, kernel, stride, padding, groups)
 
     def run_conv1d_unroll_legacy():
-        return _conv1d_unroll_legacy(
-            in_shape, out_shape, kernel, stride, padding, groups
-        )
+        return _conv1d_unroll_legacy(in_shape, oshape, kernel, stride, padding, groups)
 
-    if not request.node.get_closest_marker("perf"):
+    if not is_ci_env():
         # 10~40x faster
-        t1 = timeit.timeit(lambda: run_conv1d_unroll(), number=5)
-        t2 = timeit.timeit(lambda: run_conv1d_unroll_legacy(), number=5)
-        print("Optimized: ", t1, "Legacy: ", t2)
+        n = 5
+        t1 = timeit.timeit(lambda: run_conv1d_unroll(), number=n)
+        t2 = timeit.timeit(lambda: run_conv1d_unroll_legacy(), number=n)
+        print("Optimized: ", t1 / n, "Legacy: ", t2 / n)
+
+    if groups > 1:
+        pytest.skip("'_conv1d_unroll_legacy()' has bugs handling groups > 1")
 
     r_opt = run_conv1d_unroll()
     r_legacy = run_conv1d_unroll_legacy()
     assert np.array_equal(r_opt, r_legacy)
+
+
+@pytest.mark.parametrize(
+    test_conv1d_unroll_data["args"], test_conv1d_unroll_data["data"]
+)
+def test_conv1d_unroll(in_shape, ci, co, ksize, stride, padding, groups, fixed_rng):
+    oshape = _conv1d_oshape(in_shape, ksize, stride, padding)
+    assert ci % groups == 0 and co % groups == 0
+    ci_in_grp = ci // groups
+
+    k_shape = (co, ci_in_grp) + ksize
+    kernel = gen_random_array(k_shape, np.int8, fixed_rng)
+
+    x = gen_random_array((ci,) + in_shape, np.uint8, fixed_rng)
+
+    y_golden = conv1d_golden(x, oshape, kernel, stride, padding, groups=groups)
+
+    k_unrolled = _conv1d_unroll(in_shape, oshape, kernel, stride, padding, groups)
+    y_unrolled = x.ravel().astype(np.int32) @ k_unrolled
+
+    assert np.array_equal(y_golden.ravel(), y_unrolled)
 
 
 try:
@@ -148,11 +159,12 @@ except ImportError:
 
 
 test_conv2d_faster_data = ParametrizedTestData(
-    args="in_shape, in_channels, out_channels, ksize, stride, padding, dilation, groups",
+    args="in_shape, co, ksize, stride, padding, dilation, groups",
     data=[
-        ((64, 64), 16, 32, (3, 3), (1, 1), (1, 1), (2, 2), 4),
-        ((32, 32), 24, 24, (4, 4), (2, 2), (2, 1), (1, 1), 2),
-        ((24, 24), 16, 8, (3, 3), (1, 2), (0, 0), (1, 1), 1),
+        ((16, 64, 64), 32, (3, 3), (1, 1), (1, 1), (2, 2), 4),
+        ((24, 32, 32), 24, (4, 4), (2, 2), (2, 1), (1, 1), 2),
+        ((16, 24, 24), 8, (3, 3), (1, 2), (0, 0), (1, 1), 1),
+        ((16, 64, 64), 32, (3, 3), (2, 2), (1, 1), (0, 0), 1),
     ],
 )
 
@@ -160,47 +172,36 @@ test_conv2d_faster_data = ParametrizedTestData(
 @pytest.mark.skipif(
     skip_conv2d_faster_test, reason="Legacy function 'conv2d_faster_legacy' is removed"
 )
-@pytest.mark.perf
 @pytest.mark.parametrize(
     test_conv2d_faster_data["args"], test_conv2d_faster_data["data"]
 )
-def test_conv2d_faster_perf(
-    in_shape,
-    in_channels,
-    out_channels,
-    ksize,
-    stride,
-    padding,
-    dilation,
-    groups,
-    fixed_rng,
-    request,
-):
-    cin_per_grp = in_channels // groups
+def test_conv2d_perf(in_shape, co, ksize, stride, padding, dilation, groups, fixed_rng):
+    ci = in_shape[0]
+    oshape = _conv2d_oshape(in_shape[1:], ksize, stride, padding, dilation)
+    assert ci % groups == 0 and co % groups == 0
+    ci_in_grp = ci // groups
 
-    x = gen_random_array((in_channels,) + in_shape, np.uint8, fixed_rng)
-    kernel = gen_random_array((out_channels, cin_per_grp) + ksize, np.int8, fixed_rng)
-
-    out_shape = (
-        (in_shape[0] + 2 * padding[0] - dilation[0] * (ksize[0] - 1) - 1) // stride[0]
-        + 1,
-        (in_shape[1] + 2 * padding[1] - dilation[1] * (ksize[1] - 1) - 1) // stride[1]
-        + 1,
-    )
+    x = gen_random_array(in_shape, np.uint8, fixed_rng)
+    kernel = gen_random_array((co, ci_in_grp) + ksize, np.int8, fixed_rng)
 
     def run_conv2d_faster():
-        return conv2d_faster(x, out_shape, kernel, stride, padding, dilation, groups)
+        return conv2d_faster(x, oshape, kernel, stride, padding, dilation, groups)
 
     def run_conv2d_faster_legacy():
         return conv2d_faster_legacy(
-            x, out_shape, kernel, stride, padding, dilation, groups
+            x, oshape, kernel, stride, padding, dilation, groups
         )
 
-    if not request.node.get_closest_marker("perf"):
+    def run_conv2d_golden():
+        return conv2d_golden(x, oshape, kernel, stride, padding, dilation, groups)
+
+    if not is_ci_env():
         # ~3x faster
-        t1 = timeit.timeit(lambda: run_conv2d_faster(), number=10)
-        t2 = timeit.timeit(lambda: run_conv2d_faster_legacy(), number=10)
-        print("Use im2col: ", t1, "Use legacy:", t2)
+        n = 10
+        t1 = timeit.timeit(lambda: run_conv2d_faster(), number=n)
+        t2 = timeit.timeit(lambda: run_conv2d_faster_legacy(), number=n)
+        t3 = timeit.timeit(lambda: run_conv2d_golden(), number=n)
+        print("Optimized: ", t1 / n, "Legacy: ", t2 / n, "Golden: ", t3 / n)
 
     r_opt = run_conv2d_faster()
     r_legacy = run_conv2d_faster_legacy()
@@ -216,11 +217,12 @@ except ImportError:
 
 
 test_conv2d_unroll_data = ParametrizedTestData(
-    args="in_shape, in_channels, out_channels, ksize, stride, padding, groups",
+    args="in_shape, ci, co, ksize, stride, padding, groups",
     data=[
         ((16, 16), 4, 16, (3, 3), (1, 1), (0, 0), 1),
-        ((32, 32), 8, 16, (3, 3), (1, 1), (1, 1), 1),
+        ((32, 32), 16, 32, (3, 3), (1, 1), (1, 1), 1),
         ((32, 32), 32, 8, (4, 4), (2, 2), (0, 0), 2),
+        ((32, 24), 16, 24, (4, 4), (2, 2), (2, 2), 8),
     ],
 )
 
@@ -229,43 +231,56 @@ test_conv2d_unroll_data = ParametrizedTestData(
     skip_conv2d_unroll_test,
     reason="Legacy function '_conv2d_unroll_legacy' is removed",
 )
-@pytest.mark.perf
 @pytest.mark.parametrize(
     test_conv2d_unroll_data["args"], test_conv2d_unroll_data["data"]
 )
 def test_conv2d_unroll_perf(
-    in_shape,
-    in_channels,
-    out_channels,
-    ksize,
-    stride,
-    padding,
-    groups,
-    fixed_rng,
-    request,
+    in_shape, ci, co, ksize, stride, padding, groups, fixed_rng
 ):
-    out_shape = (
-        (in_shape[0] + 2 * padding[0] - ksize[0]) // stride[0] + 1,
-        (in_shape[1] + 2 * padding[1] - ksize[1]) // stride[1] + 1,
-    )
+    oshape = _conv2d_oshape(in_shape, ksize, stride, padding)
+    assert ci % groups == 0 and co % groups == 0
+    ci_in_grp = ci // groups
 
-    k_shape = (out_channels, in_channels) + ksize
+    k_shape = (co, ci_in_grp) + ksize
     kernel = gen_random_array(k_shape, np.int8, fixed_rng)
 
     def run_conv2d_unroll():
-        return _conv2d_unroll(in_shape, out_shape, kernel, stride, padding, groups)
+        return _conv2d_unroll(in_shape, oshape, kernel, stride, padding, groups)
 
     def run_conv2d_unroll_legacy():
-        return _conv2d_unroll_legacy(
-            in_shape, out_shape, kernel, stride, padding, groups
-        )
+        return _conv2d_unroll_legacy(in_shape, oshape, kernel, stride, padding, groups)
 
-    if not request.node.get_closest_marker("perf"):
+    if not is_ci_env():
         # 50~100x faster
-        t1 = timeit.timeit(lambda: run_conv2d_unroll(), number=5)
-        t2 = timeit.timeit(lambda: run_conv2d_unroll_legacy(), number=5)
-        print("Optimized: ", t1, "Legacy: ", t2)
+        n = 5
+        t1 = timeit.timeit(lambda: run_conv2d_unroll(), number=n)
+        t2 = timeit.timeit(lambda: run_conv2d_unroll_legacy(), number=n)
+        print("Optimized: ", t1 / n, "Legacy: ", t2 / n)
+
+    if groups > 1:
+        pytest.skip("'_conv2d_unroll_legacy()' has bugs handling groups > 1")
 
     r_opt = run_conv2d_unroll()
     r_legacy = run_conv2d_unroll_legacy()
     assert np.array_equal(r_opt, r_legacy)
+
+
+@pytest.mark.parametrize(
+    test_conv2d_unroll_data["args"], test_conv2d_unroll_data["data"]
+)
+def test_conv2d_unroll(in_shape, ci, co, ksize, stride, padding, groups, fixed_rng):
+    oshape = _conv2d_oshape(in_shape, ksize, stride, padding)
+    assert ci % groups == 0 and co % groups == 0
+    ci_in_grp = ci // groups
+
+    k_shape = (co, ci_in_grp) + ksize
+    kernel = gen_random_array(k_shape, np.int8, fixed_rng)
+
+    x = gen_random_array((ci,) + in_shape, np.uint8, fixed_rng)
+
+    y_expected = conv2d_faster(x, oshape, kernel, stride, padding, groups=groups)
+
+    k_unrolled = _conv2d_unroll(in_shape, oshape, kernel, stride, padding, groups)
+    y_unrolled = (x.ravel().astype(np.int32) @ k_unrolled).astype(np.int32)
+
+    assert np.array_equal(y_expected.ravel(), y_unrolled)
