31f2ba3e6f1bed434232a992ec08a3f2b9f8cfc7
KafCoppelia <k740677208@gmail.com>
2025-10-20 15:41:43 +0800

    ✨ feat(runtime): add online core support & improve frame encoding/decoding

--- paibox/runtime/__init__.py
+++ paibox/runtime/__init__.py
@@ -10,17 +10,19 @@ except ImportError:
 del paicorelib
 
 # Version check for standablone scenario
-MAX_PLIB_VERSION = "1.5.0"
-from paicorelib import __version__ as plib_version
+# In case of breaking changes in paicorelib, maximum allowed version is set here.
+MAX_SUPPORT_PLIB_VER = "1.5.0"
+from paicorelib import __version__ as plib_ver
+from packaging import version as pkg_version
 
-if plib_version is not None:
-    if plib_version >= MAX_PLIB_VERSION:
+if plib_ver is not None:
+    if pkg_version.parse(plib_ver) >= pkg_version.parse(MAX_SUPPORT_PLIB_VER):
         raise ImportError(
-            f"The runtime requires paicorelib version < {MAX_PLIB_VERSION}, but {plib_version} is installed."
+            f"The runtime only support paicorelib version < {MAX_SUPPORT_PLIB_VER}, but {plib_ver} is installed."
         ) from None
 else:
     warnings.warn(
-        f"No exact version found, make sure the paicorelib version < {MAX_PLIB_VERSION}."
+        f"No exact version found, make sure the paicorelib version < {MAX_SUPPORT_PLIB_VER}."
     )
 
 from .runtime import PAIBoxRuntime
