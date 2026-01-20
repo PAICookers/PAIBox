47e365e868b96b8d9889a63028c2b8313e44c468
KafCoppelia <k740677208@gmail.com>
2026-01-20 11:45:14 +0800

    feat(fuse): add custom `fuse_conv_bn` function instead of using the `fuse()` of `fx.experimental.optimization`

--- paibox/fx_converter/fuse.py
+++ paibox/fx_converter/fuse.py
@@ -2,9 +2,12 @@ import copy
 from collections.abc import Callable
 from typing import Any
 
-from torch import fx
-from torch.fx.experimental.optimization import fuse as fuse_conv_bn
-from torch.fx.experimental.optimization import matches_module_pattern
+from torch import fx, nn
+from torch.fx.experimental.optimization import (
+    matches_module_pattern,
+    replace_node_module,
+)
+from torch.nn.utils.fusion import fuse_conv_bn_eval, fuse_linear_bn_eval
 
 from paibox import _logging
 
@@ -18,6 +21,51 @@ from .opset import (
 )
 
 fuse_log = _logging.get_artifact_logger(__name__, "fuse")
+
+
+# NOTE: in case of the arguments of fuse() is changing, define it here.
+def fuse_conv_bn(
+    model: nn.Module, inplace: bool = False, no_trace: bool = False
+) -> nn.Module:
+    """
+    Fuses convolution/BN and linear/BN layers for inference purposes.
+    Will deepcopy your model by default, but can modify the model inplace as well.
+    """
+    patterns = [
+        (nn.Conv1d, nn.BatchNorm1d),
+        (nn.Conv2d, nn.BatchNorm2d),
+        (nn.Conv3d, nn.BatchNorm3d),
+        (nn.Linear, nn.BatchNorm1d),
+    ]
+    if not inplace:
+        model = copy.deepcopy(model)
+    if not no_trace or not isinstance(model, fx.GraphModule):
+        fx_model = fx.symbolic_trace(model)
+    else:
+        fx_model = model
+    modules = dict(fx_model.named_modules())
+    new_graph = copy.deepcopy(fx_model.graph)
+
+    for pattern in patterns:
+        for node in new_graph.nodes:
+            if matches_module_pattern(pattern, node, modules):
+                if len(node.args[0].users) > 1:
+                    # Output of conv/linear is used by other nodes
+                    continue
+                first_layer = modules[node.args[0].target]
+                bn = modules[node.target]
+                if not bn.track_running_stats:
+                    continue
+                if pattern[0] in [nn.Conv1d, nn.Conv2d, nn.Conv3d]:
+                    fused_layer = fuse_conv_bn_eval(first_layer, bn)
+                else:  # nn.Linear
+                    fused_layer = fuse_linear_bn_eval(first_layer, bn)
+                replace_node_module(node.args[0], modules, fused_layer)
+                node.replace_all_uses_with(node.args[0])
+                new_graph.erase_node(node)
+
+    new_graph.lint()
+    return fx.GraphModule(fx_model, new_graph)
 
 
 def _parent_name(target: str) -> tuple[str, str]:
